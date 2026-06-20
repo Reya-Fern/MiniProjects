0c10f046266deace0903bac46abde79d75fb21a2
Reya-Fern <ry.wannipa@gmail.com>
2026-06-20 22:52:52 +0700

    Implement thumpView

--- MiniProjects/Projects/FocusPlant/View/CircularSliderView.swift
+++ MiniProjects/Projects/FocusPlant/View/CircularSliderView.swift
@@ -8,13 +8,23 @@
 import UIKit
 
 final class CircularSliderView: UIView {
+    private var progress:CGFloat = 0.75
     private let trackLayer = CAShapeLayer()
     private let progressLayer = CAShapeLayer()
+    private let thumpView = UIView()
+    private var didSetupLayers = false
 
     override func layoutSubviews() {
         super.layoutSubviews()
 
-        configureLayer()
+        if !didSetupLayers {
+
+            configureLayer()
+            configureThump()
+
+            didSetupLayers = true
+        }
+        updateThumpPosition()
     }
 }
 
@@ -46,7 +56,34 @@ extension CircularSliderView {
         layer.addSublayer(progressLayer)
     }
 
+    private func configureThump() {
+        thumpView.frame = CGRect(x: 0, y: 0, width: 24, height: 24)
+        thumpView.layer.cornerRadius = thumpView.bounds.width / 2
+        thumpView.backgroundColor = .white
+
+        thumpView.layer.shadowColor = UIColor.black.cgColor
+        thumpView.layer.shadowOpacity = 0.15
+        thumpView.layer.shadowRadius = 6
+        thumpView.layer.shadowOffset = .init(width: 0, height: 2)
+
+        addSubview(thumpView)
+    }
+
+    private func updateThumpPosition() {
+        let center = CGPoint(x: bounds.midX, y: bounds.midY)
+        let radius = min(bounds.width, bounds.height) / 2
+        let angle = (-CGFloat.pi / 2) + (progress * 2 * .pi)
+        let x = center.x + radius * cos(angle)
+        let y = center.y + radius * sin(angle)
+
+        thumpView.center = CGPoint(x: x, y: y)
+    }
+
     func setProgress(_ progress: CGFloat) {
-        progressLayer.strokeEnd = progress
+        self.progress = max(0, min(progress, 1))
+
+        progressLayer.strokeEnd = self.progress
+
+        updateThumpPosition()
     }
 }
